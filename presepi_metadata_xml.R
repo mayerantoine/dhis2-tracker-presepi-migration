c11d67bdc40263094c07d65616c1da79890d9acf
Antoine <mayerantoine@gmail.com>
2017-12-07 16:57:21 -0500

    Initial Commit

---
+++ presepi_metadata_xml.R
@@ -0,0 +1,67 @@
+library(XML)
+library(tidyverse)
+
+rm(list = ls())
+filename <- "metadata_presepi_201709.xml"
+doc <- xmlTreeParse(filename,useInternalNodes = T)
+rootNode <- xmlRoot(doc)
+xmlName(rootNode)
+dataElement <- rootNode[["dataElements"]]
+program <- rootNode[["programs"]]
+TrackedEntityAttribute <- rootNode[["trackedEntityAttributes"]]
+programStage <- program[[2]][["programStages"]]
+
+programTrackedEntityAttribute <- program[[2]][["programTrackedEntityAttributes"]]
+orgunits <- program[[2]][["organisationUnits"]]
+
+## programs
+ProgramId <- as.list(xmlSApply(program, function(x) xmlGetAttr(x,"id")))
+ProgramName <- as.list(xmlSApply(program, function(x) xmlGetAttr(x,"name")))
+tb_program <-  do.call(rbind, Map(data.frame, Id=ProgramId, name=ProgramName))
+
+#programStages
+ProgramStageId <- as.list(xmlSApply(programStage, function(x) xmlGetAttr(x,"id")))
+ProgramStageName <- as.list(xmlSApply(programStage, function(x) xmlGetAttr(x,"name")))
+tb_programStage <-  do.call(rbind, Map(data.frame, Id=ProgramStageId, name=ProgramStageName))
+
+#TrackedEntityAttribute
+TrackedEntityAttributeId <- as.list(xmlSApply(TrackedEntityAttribute, function(x) xmlGetAttr(x,"id")))
+TrackedEntityAttributeName <- as.list(xmlSApply(TrackedEntityAttribute, function(x) xmlGetAttr(x,"name")))
+TrackedEntityAttributeCode <- as.list(xmlSApply(TrackedEntityAttribute, function(x)
+{ ifelse ( is.null( xmlGetAttr(x,"code")),"NONE",xmlGetAttr(x,"code"))
+}  ))
+
+tb_TrackedEntityAttribute <- do.call(rbind,Map(data.frame, Id=TrackedEntityAttributeId,code=TrackedEntityAttributeCode, name=TrackedEntityAttributeName))
+
+#programTrackedEntity
+programTrackedEntityAttributeId <- as.list(xmlSApply(programTrackedEntityAttribute, function(x) xmlGetAttr(x,"id")))
+programTrackedEntityAttributeName <- as.list(xmlSApply(programTrackedEntityAttribute, function(x) xmlGetAttr(x,"name")))
+tb_programTrackedEntityAttribute <-  do.call(rbind, Map(data.frame, Id=TrackedEntityAttributeId, name=TrackedEntityAttributeName))
+write_csv(tb_programTrackedEntityAttribute,"tb_programTrackedEntityAttribute.csv")
+
+#orgunits
+orgunitsId <- as.list(xmlSApply(orgunits, function(x) xmlGetAttr(x,"id")))
+orgunitsName <- as.list(xmlSApply(orgunits, function(x) xmlGetAttr(x,"name")))
+tb_orgunits <-  do.call(rbind, Map(data.frame, Id=orgunitsId, name=orgunitsName))
+
+
+#dataElements
+dataElementId <- as.list(xmlSApply(dataElement, function(x) xmlGetAttr(x,"id")))
+dataElementName <- as.list(xmlSApply(dataElement, function(x) xmlGetAttr(x,"name")))
+#dataElementCode <- as.list(xmlSApply(dataElement, function(x) xmlGetAttr(x,"code")))
+dataElementDomainType <- as.list(xmlSApply(dataElement, function(x) xmlValue(x[["domainType"]])))
+tb_dataElement <- do.call(rbind, Map(data.frame, Id=dataElementId, name=dataElementName,domain=dataElementDomainType))
+write_csv(tb_dataElement,"tb_dataElement.csv")
+
+
+####LOAD MAIN DATA
+main_data <- read_tsv("main_data.txt",col_names = T)
+main_data_col <- as.data.frame(names(main_data))
+names(main_data_col) <- "presepi_variables"
+
+presepi_sites <- distinct(main_data[,c("SiteName")])
+presepi_sites <- presepi_sites[!is.na(presepi_sites$SiteName),]
+
+
+
+
---
+++ query_presepi_metadata.R
@@ -0,0 +1,11 @@
+## Programs --------------------------------------------
+# http://209.61.231.45:8082/dhis/api/programs?&fields=id,name&links=false&paging=false
+
+## PRESEPI Program
+#http://209.61.231.45:8082/dhis/api/programs/ybHHvBdo1ke?fields=id,name,programTrackedEntityAttributes[id,name,code],organisationUnits[id,name],programStages[id,name]
+
+## all ProgramStage --------------------------------------------
+# http://209.61.231.45:8082/dhis/api/programStages?&fields=id,name&links=false&paging=false
+
+## dataElements --------------------------------------------------
+# http://209.61.231.45:8082/dhis/api/dataElements?fields=id,name,domainType,code&links=false&paging=false
