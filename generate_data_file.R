0dd0494bbce4c3d857a7195d67cbaed0ad8db709
Antoine <mayerantoine@gmail.com>
2018-04-30 20:06:47 -0400

    Fix Missing DataElement

--- generate_data_file.R
+++ generate_data_file.R
@@ -21,8 +21,35 @@ source("query_presepi_metadata.R")
 loadMainData <- function() {
     # main_data table
     df_main_data <-
-        read_tsv("./data/main_data_final.txt", col_names = T) %>%
+        read_tsv("./data/main_data_final.txt", col_names = T) 
+    
+     df_main_completed <-
+        read_tsv("./data/main_completed.txt", col_names = T) 
+     
+     df_main_vaccinal <-
+        read_tsv("./data/main_status_vaccinal.txt", col_names = T) 
+     
+     
+     df_main_completed$PTID <- toupper(df_main_completed$PTID)
+     
+     df_main_vaccinal$PTID <- toupper(df_main_vaccinal$PTID)
+   
+     df_main_completed <- df_main_completed %>% select(-DateAj,-DateFrm,-DOB,-DeptVive,-AbxDiar)    
+     
+     df_main_vaccinal <- df_main_vaccinal %>% select(-DateAj,-DateFrm,-VD1rota,-VacPEV,-VD3rota,-VacRota,
+                                                     -VD2rota,-VsrcRota,-VacChol,-VNDChol,-VD1Chol,-VNDrota,
+                                                     -VD2Chol,-VD3Chol,-VsrcChol,
+                                                     -VacMeng,-VNDMeng,-VD1Meng,-VD2Meng,-VD3Meng,-VsrcMeng,
+                                                     -VacPent,-VNDPent,-VD1Pent,-VD2Pent,-VD3Pent,-VsrcPent,-VacCopie)
+                    
+     df_main_data <- df_main_data %>% left_join(df_main_completed,by= c("PTID"="PTID"))
+     
+     df_main_data <- df_main_data %>% left_join(df_main_vaccinal, by=c("PTID"="PTID"))
+      
+     # cleaning
+    df_main_data <- 
         
+        df_main_data %>%  
         # remove site with no code
         filter(!is.na(SiteCode)) %>%
         
@@ -35,9 +62,57 @@ loadMainData <- function() {
         #include presepi dhis2 siteId
         left_join(df_orgunits, by = c("site_dhis2" = "name")) %>%
         
-        ## year
-        mutate(year_case = year(mdy(DateFrm)), month_case = month(mdy(DateFrm)))
+        ## year and Date
+        mutate( DateFrm = mdy(DateFrm), 
+                DateAj= mdy(DateAj),
+                DateMod = mdy(DateMod),
+                DateVisit = mdy(DateVisit),
+                SpecDate = mdy(SpecDate),
+                year_case = year(DateFrm), 
+                month_case = month(DateFrm)) %>%
     
+        ## AntibioticAdmin
+        mutate(AntibioticAdmin = if_else(Doxy == -1,1,
+                                         if_else(Azithrom == -1,1,
+                                                 if_else(Erythrom == -1,1,
+                                                         if_else(Tetracyc == -1,1,
+                                                                 if_else(Amox == -1,1,
+                                                                         if_else(Cipro == -1,1,
+                                                                            if_else(TmpSmx == -1,1,0)
+                                                                         )
+                                                                 )
+                                                         )
+                                                 )
+                                         ))
+               ) %>%
+
+     
+       ## EtatdesSelles
+        mutate(EtatdesSelles = if_else(DiarAqus != 0,1,
+                                         if_else(DiarSang !=  0,1,
+                                                 if_else(DiarGlar != 0,1,
+                                                         if_else(DiarAcun != 0,1,0)
+                                                        )
+                                                 )
+                                         )
+               ) %>%
+         
+         #PlanTraitHydratation
+        mutate(PlanTraitHydratation = if_else(is.na(PlanTrait),0,1)) %>%
+         #PlanA
+        mutate(PlanA = if_else(PlanTrait == 0,1,0)) %>% 
+         #PlanB
+        mutate(PlanB = if_else(PlanTrait == 1,1,0)) %>% 
+         #PlanC
+        mutate(PlanC = if_else(PlanTrait == 2,1,0)) %>% 
+           #syndromeDiar
+        mutate(SyndDiar = if_else( Syndrome == 1 || SyndDiar == -1,1,0)) %>%
+         #syndromeFeb
+         mutate(SyndFebrile = if_else( Syndrome == 2 || SyndFebrile == -1,1,0)) %>% 
+         #syndromeInfRes
+         mutate(SyndInfResp = if_else( Syndrome == 3 || SyndInfResp == -1,1,0))
+         
+
     
     df_main_data
     # main data complement table
@@ -59,7 +134,7 @@ getVariableMapping <- function() {
     # variables mapping table
     # This is the most important input for the data generation process, match presepi variable names, dhis names
     df_mapping <-
-        read_csv("./data/presepi_mapping_v2.csv", col_names = T)
+        read_csv("./data/presepi_mapping_v3.csv", col_names = T)
     
     ## clean the mapping ---------------------------------------------------------------------------
     
@@ -75,7 +150,7 @@ getVariableMapping <- function() {
         mutate(dataelement = trimws(dataelement, which = c("right"))) %>%
         
         #exclude presepi variable that are not mapped in dhis2 those data will not be transfered
-        filter(!is.na(dataelement)) %>%
+        filter(!is.na(code)) %>%
         
         #keeping only variables in presepi that we can find in the main data
         filter(VariableName %in% names(df_main_data)) %>%
@@ -83,7 +158,7 @@ getVariableMapping <- function() {
         # adding dataelement Ids in the mapping dataframe
         left_join(df_metadata, by = c("dataelement" = "name")) %>%
         
-        #exclude presepi variable that are not mapped
+        #exclude presepi variable that are not mapped??? remove element with no code
         filter(!is.na(Id)) %>%
         
         # add ProgramStageId in the mapping dataframe, indicates the stage  for each dataelement
--- getDataList.R
+++ getDataList.R
@@ -1,132 +1,149 @@
 
-    
- getDataList <-  function(tr_main_data)   {  
-     
-    main_list <-  map(tr_main_data,function(x){
-        instance <-  list( instance_attribute = list(orgunit = as.character(x[["orgunitsId"]]),
-                                                     enrollmentdate=mdy(x[["DateFrm"]])),
-                           attributes = list(),
-                           ProgramStages= list())
+
+
+getDataList <-  function(tr_main_data)   {
+    main_list <-  map(tr_main_data, function(x) {
+        instance <-
+            list(
+                instance_attribute = list(orgunit = as.character(x[["orgunitsId"]]),
+                                          enrollmentdate = x[["DateFrm"]]),
+                attributes = list(),
+                ProgramStages = list()
+            )
         
-          tr_mapping_attr <- df_mapping %>% 
+        tr_mapping_attr <- df_mapping %>%
             filter(tracker_type == "Attribute") %>%
-              select(VariableName,Id) %>%
-                        transpose()
-          
-        instance[["attributes"]] <-  map(tr_mapping_attr, function(y){
-              
-              var_name <- y[["VariableName"]]
-              attribute <- list(id=y[["Id"]], value=x[[var_name]], name =y[["VariableName"]] )
-              
-          })
+            select(VariableName, Id) %>%
+            transpose()
         
-         tr_programStage <- df_programStages %>%
-                        filter(name %in% c("QUESTIONNAIRE DESTINÉ AU PATIENT",
-                                           "RESULTAT POUR SELLES",
-                                           "RESULTAT POUR SERUM",
-                                           "RESULTAT POUR LIQUIDE PLEURAL")) %>%
-                        select(name,Id) %>%
-                        transpose()
-         
-        instance[["ProgramStages"]] <- map(tr_programStage, function(v){
-         ProgramStage <- list( program_stage_attribute = 
-                                       list(id =v[["Id"]], name=v[["name"]]),
-                                    events =
-                                        list(attrs = list(), dataValues = list()) )
-             
-             
-               stageId <- v[["Id"]]
-             # create stage data elements 
-             tr_mapping_dataElement <- df_mapping %>% 
-            filter(tracker_type == "dataElement", programStage == stageId) %>%
-              select(VariableName,Id) %>%
-                        transpose()
-             
-             ProgramStage[["events"]][["attrs"]] <- 
-                 list(program =presePiId,orgunit = as.character(x[["orgunitsId"]]), eventDate = mdy(x[["DateFrm"]]) )
-             
-             ProgramStage[["events"]][["dataValues"]] <-  map(tr_mapping_dataElement, function(s){
-             dataElment_name <- s[["VariableName"]]
-            dataValue <- list(id=s[["Id"]], value=x[[dataElment_name]], name = s[["VariableName"]])
-                 
-             })
-             
-             ProgramStage
-         })
+        instance[["attributes"]] <-
+            map(tr_mapping_attr, function(y) {
+                var_name <- y[["VariableName"]]
+                attribute <-
+                    list(id = y[["Id"]],
+                         value = x[[var_name]],
+                         name = y[["VariableName"]])
+                
+            })
+        
+        tr_programStage <- df_programStages %>%
+            filter(
+                name %in% c(
+                    "QUESTIONNAIRE DESTINÉ AU PATIENT",
+                    "RESULTAT POUR SELLES",
+                    "RESULTAT POUR SERUM",
+                    "RESULTAT POUR LIQUIDE PLEURAL"
+                )
+            ) %>%
+            select(name, Id) %>%
+            transpose()
+        
+        instance[["ProgramStages"]] <-
+            map(tr_programStage, function(v) {
+                ProgramStage <- list(
+                    program_stage_attribute =
+                        list(id = v[["Id"]], name = v[["name"]]),
+                    events =
+                        list(attrs = list(), dataValues = list())
+                )
+                
+                
+                stageId <- v[["Id"]]
+                # create stage data elements
+                tr_mapping_dataElement <- df_mapping %>%
+                    filter(tracker_type == "dataElement",
+                           programStage == stageId) %>%
+                    select(VariableName, Id) %>%
+                    transpose()
+                
+                ProgramStage[["events"]][["attrs"]] <-
+                    list(
+                        program = presePiId,
+                        orgunit = as.character(x[["orgunitsId"]]),
+                        eventDate = x[["DateFrm"]]
+                    )
+                
+                ProgramStage[["events"]][["dataValues"]] <-
+                    map(tr_mapping_dataElement, function(s) {
+                        dataElment_name <- s[["VariableName"]]
+                        dataValue <-
+                            list(id = s[["Id"]],
+                                 value = x[[dataElment_name]],
+                                 name = s[["VariableName"]])
+                        
+                    })
+                
+                ProgramStage
+            })
         
         instance
-         
-         
-     })
- }
- 
+        
+        
+    })
+}
 
- 
- 
- 
- generateFile <- function(){
- 
-     
-  import_group <-  df_main_data %>% 
-     filter(!is.na(DateFrm),!is.na(orgunitsId)) %>%
-     mutate(new_DateFrm = mdy(DateFrm)) %>%
-     mutate(year_case =  year(new_DateFrm), month_case = month(new_DateFrm)) %>%
-     group_by(SiteCode, year_case,month_case) %>% 
-     summarise(n= n())  %>% 
-     arrange(SiteCode, desc(year_case), month_case)
+
+
+
+
+generateFile <- function() {
+    import_group <-  df_main_data %>%
+        filter(!is.na(DateFrm), !is.na(orgunitsId)) %>%
+        group_by(SiteCode, year_case, month_case) %>%
+        summarise(n = n())  %>%
+        arrange(SiteCode, desc(year_case), month_case)
     
-        code <- "HSD"
-         year_c <- 2017
-         month_c <- 9
-  
+    code <- "HSD"
+    year_c <- 2017
+    month_c <- 10
+    
     tr_main_data_test <- df_main_data %>%
         filter(!is.na(DateFrm)) %>%
         # test one site
-        filter(SiteCode == code,year_case == year_c, month_case == month_c) %>%
+        filter(SiteCode == code, year_case == year_c, month_case == month_c) %>%
         # transpose to list
-        transpose() 
-        
-         batch_list <- getDataList(tr_main_data_test)
-         batch_json <- toJSON(batch_list,pretty = T)
-         
-         filename <- paste(code,year_c,month_c, sep = "_")
+        transpose()
     
-         dirfile <- paste0("processed_data/output/",filename,".json")
-         write(batch_json,dirfile)  
+    batch_list <- getDataList(tr_main_data_test)
+    batch_json <- toJSON(batch_list, pretty = T)
+    
+    filename <- paste(code, year_c, month_c, sep = "_")
+    
+    dirfile <- paste0("processed_data/output/", filename, ".json")
+    write(batch_json, dirfile)
     
     
-     # import_group_tr <- import_group %>% transpose()
-     # 
-     # map(import_group_tr, function(x){
-     #     
-     #     code <- x[["SiteCode"]]
-     #     year_c <- x[["year_case"]]
-     #     month_c <- x[["month_case"]]
-     #     
-     #     
-     #    tr_main_data_test <- df_main_data %>%
-     #    filter(!is.na(DateFrm)) %>%
-     #    # test one site
-     #    #filter(SiteCode == code,year_case == year_c, month_case == month_c) %>%
-     #    filter(SiteCode == "HSD",year_case == 2017, month_case == 9) %>%
-     #    # transpose to list
-     #    transpose() 
-     #    
-     #     batch_list <- getDataList(tr_main_data_test)
-     #     batch_json <- toJSON(batch_list,pretty = T)
-     #     
-     #     filename <- paste(code,year_c,month_c, sep = "_")
-     #     dirfile <- paste0("processed_data/output/",filename,".json")
-     #     write_json(batch_json,dirfile)
-     # 
-     #     
-     # })
- }
- 
-    
-      
-     
-     
+    # import_group_tr <- import_group %>% transpose()
+    #
+    # map(import_group_tr, function(x){
+    #
+    #     code <- x[["SiteCode"]]
+    #     year_c <- x[["year_case"]]
+    #     month_c <- x[["month_case"]]
+    #
+    #
+    #    tr_main_data_test <- df_main_data %>%
+    #    filter(!is.na(DateFrm)) %>%
+    #    # test one site
+    #    #filter(SiteCode == code,year_case == year_c, month_case == month_c) %>%
+    #    filter(SiteCode == "HSD",year_case == 2017, month_case == 9) %>%
+    #    # transpose to list
+    #    transpose()
+    #
+    #     batch_list <- getDataList(tr_main_data_test)
+    #     batch_json <- toJSON(batch_list,pretty = T)
+    #
+    #     filename <- paste(code,year_c,month_c, sep = "_")
+    #     dirfile <- paste0("processed_data/output/",filename,".json")
+    #     write_json(batch_json,dirfile)
+    #
+    #
+    # })
+}
 
 
- 
+
+
+
+
+
